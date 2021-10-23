f4dcb284372b9f3fdc0f07bd8fecd77f6d76d3d2
zakariahajja <zakaria.hajja@alphanetworks.tv>
2021-10-24 00:27:44 +0100

    #add helper class resource

---
+++ cryptocurrencychallenge/src/main/java/com/yoti/android/cryptocurrencychallenge/commons/Resource.kt
@@ -0,0 +1,18 @@
+package com.yoti.android.cryptocurrencychallenge.commons
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val cause: Exception.Cause? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T?) : Resource<T>(data = data)
+    class Exception<T>(cause: Cause?) : Resource<T>(cause = cause) {
+        sealed class Cause {
+            object NoInternetConnectionCause : Cause()
+            object HttpCause : Cause()
+            object UnknownCause : Cause()
+        }
+    }
+
+    class Loading<T>() : Resource<T>()
+}
